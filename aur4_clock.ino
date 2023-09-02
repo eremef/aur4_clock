0307a6fc9f73f1a84f42a72ac979844fe09d53f8
eremef <34741103+eremef@users.noreply.github.com>
2023-09-03 01:30:16 +0200

    Update aur4_clock.ino

--- aur4_clock.ino
+++ aur4_clock.ino
@@ -5,16 +5,17 @@
 
   Created using the Arduino Uno R4 Wifi example code - RTC_NTPSync, initially created by Sebastian Romero @sebromero  
 
- * Instructions:
- * 1. Change the WiFi credentials in the arduino_secrets.h file to match your WiFi network.
- * 2. Set the orientation using the #define ORIENTATION 0 or 1
+ Instructions:
+ 1. Change the WiFi credentials in the arduino_secrets.h file to match your WiFi network.
+ 2. Set the orientation using the #define ORIENTATION 0 or 1
+ 3. Set timezone offset hours using the #define TIMEZONE_OFFSET_HOURS according to your localization
 */
 
 #include "led-matrix.h"
 #include "Arduino_LED_Matrix.h"
 #include "RTC.h"
 #include <WiFiS3.h>
-#include "arduino_secrets.h"
+#include "D:\arduino_secrets.h"
 
 #define TIMEZONE_OFFSET_HOURS 2
 #define ORIENTATION 1 // 0 (up is where the ESP32 is), 1 (up is where the Qwiic is)
